1ef507028f79677328c23defdf01479626fd45cb
Sukma Bagus Wahasdwika <sukmabaguswahasdwika10@gmail.com>
2024-10-20 00:43:20 +0700

    home_screen logo.pt

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
 import 'pages/camera_page.dart';
+import 'screens/home_screen.dart';
 
 void main() {
   runApp(MaterialApp(
     initialRoute: '/',
     routes: {
-      '/': (context) => HomePage(),
+      '/': (context) => HomeScreen(), // Halaman pertama yang ditampilkan adalah HomeScreen
       '/camera': (context) => CameraPage(),
     },
   ));
